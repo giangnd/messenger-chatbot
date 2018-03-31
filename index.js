220adad9462328a82bfce3de3b6b38155bc3e5ab
Nguyễn Đình Giang <dinhgiangbg91@gmail.com>
2018-03-31 12:44:47 +0700

    commit

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.post('/webhook/', function (req, res) {
             sendMessageByUserReg(event.recipient.id, messageData, res)
         }
     }
-    res.sendStatus(200).end()
+    res.status(200).send()
 })
 
 // recommended to inject access tokens as environmental variables, e.g.
