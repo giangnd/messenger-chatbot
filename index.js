f016b3843840a0d7d7668905287d6e8de7e7b2a0
Nguyễn Đình Giang <dinhgiangbg91@gmail.com>
2018-04-03 14:06:14 +0700

    commit

--- index.js
+++ index.js
@@ -42,7 +42,8 @@ app.post('/discount/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'crowdbotics') {
+    console.log(req.query);
+    if (req.query['hub.verify_token'] === 'magixshop') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
