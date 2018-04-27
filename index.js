1d21a54a433ba947264270e08dac96a8f47dc8de
Nguyễn Đình Giang <dinhgiangbg91@gmail.com>
2018-04-27 09:46:05 +0700

    commit

--- index.js
+++ index.js
@@ -29,140 +29,185 @@ const messageDiscount = "MagixShop congrats you on activating your discount! Ent
 
 // Index route
 app.get('/', function (req, res) {
-    res.render("index", {
-        app_id: APP_ID,
-        page_id: PAGE_ID,
-        ref: 'magixshop',
-        user_ref: Date.now()
-    })
+	res.render("index", {
+		app_id: APP_ID,
+		page_id: PAGE_ID,
+		ref: 'magixshop',
+		user_ref: Date.now()
+	})
 })
 
 app.post('/discount/', function (req, res) {
-    let user_ref = req.body.user_ref
-    sendMessageByUserReg(user_ref, { text: messageDiscount }, res)
+	let user_ref = req.body.user_ref
+	sendMessageByUserReg(user_ref, { text: messageDiscount }, res)
 })
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    console.log(req.query);
-    if (req.query['hub.verify_token'] === 'magixshop') {
-        res.send(req.query['hub.challenge'])
-    }
-    res.end('Error, wrong token')
+	console.log(req.query);
+	if (req.query['hub.verify_token'] === 'magixshop') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.end('Error, wrong token')
 })
 
 // to post data
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        console.log(event)
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		console.log(event)
 
-        if(event.sender){
-            let sender = event.sender.id
-            if (event.message && event.message.text && !event.message.is_echo) {
-                let text = event.message.text
-                if (text === 'Generic') {
-                    const desc = "Hi there! We noticed there was an item left in your shopping cart. If you're ready to complete your order, your cart is waiting for your return."
-                    sendTextMessage(sender, desc)
-                    // sendGenericMessage(sender)
-                    continue
-                }
-                if(text === 'My Abandoned Cart'){
-                    sendTextMessage(sender, "This is an example of a reminder message that your customers will see if they abandon their carts.")
-                    continue
-                }
-            }
-        }
-    }
-    res.sendStatus(200)
+		if(event.sender){
+			let sender = event.sender.id
+			if (event.message && event.message.text && !event.message.is_echo) {
+				let text = event.message.text
+				if (text.toLowerCase() === 'my cart' || text === 'PRODUCTS_PAYLOAD') {
+					sendGenericMessage(sender)
+					continue
+				}
+				if(text.toLowerCase() === 'menu'){
+					sendMenuMessage(sender)
+					continue
+				}
+				if(text === 'CONTACT_PAYLOAD'){
+					sendTextMessage(sender, "Magix Shop - 60 Paya Lebar Road, Singapore 409051")
+					continue
+				}
+			}
+			if (event.postback) {
+				let text = JSON.stringify(event.postback)
+				sendTextMessage(sender, text.substring(0, 200), token)
+				continue
+			}
+		}
+	}
+	res.sendStatus(200)
 })
 
 // recommended to inject access tokens as environmental variables, e.g.
 function sendTextMessage(sender, text) {
-    let messageData = { text: text }
-    sendMessageByUserId(sender, messageData)
+	let messageData = { text: text }
+	sendMessageByUserId(sender, messageData)
 }
 
 function sendGenericMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [
-                {
-                    "title": "Flower Piping Tips",
-                    "subtitle": "1 x $35.00 USD",
-                    "image_url": "https://cdn.shopify.com/s/files/1/2330/6765/products/maxresdefault-1080x675_419x.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://magixshop.com/",
-                        "title": "Shop Now",
-                    }],
-                },
-                {
-                    "title": "Flower Piping Tips",
-                    "subtitle": "1 x $35.00 USD",
-                    "image_url": "https://cdn.shopify.com/s/files/1/2330/6765/products/maxresdefault-1080x675_419x.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://magixshop.com/",
-                        "title": "Shop Now",
-                    }],
-                }
-                ]
-            }
-        }
-    }
-    sendMessageByUserId(sender, messageData)
+	let messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [
+				{
+					"title": "USB Blender Bottle 2018",
+					"subtitle": "1 x $49.98",
+					"image_url": "https://cdn.shopify.com/s/files/1/2330/6765/products/USB-Blender-Bottle-2018-thumbnail_1_500x.jpg",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://magixshop.com/collections/other-gadgets/products/usb-blender-bottle-2018",
+						"title": "Shop Now",
+					}],
+				},
+				{
+					"title": "Quick Kebab Maker Box",
+					"subtitle": "1 x $32.99",
+					"image_url": "https://cdn.shopify.com/s/files/1/2330/6765/products/Quick-Kebab-Maker-Box-thumbnail_13_500x.jpg",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://magixshop.com/collections/kitchen-gadgets/products/quick-kebab-maker-box",
+						"title": "Shop Now",
+					}],
+				}
+				]
+			}
+		}
+	}
+	sendMessageByUserId(sender, messageData)
+}
+
+function sendMenuMessage(sender){
+	let messageData = {
+		{
+			"persistent_menu":[{
+				"locale":"default",
+				"composer_input_disabled": true,
+				"call_to_actions":[
+				{
+					"title":"Shop Info",
+					"type":"nested",
+					"call_to_actions":[{
+						"title":"Products",
+						"type":"postback",
+						"payload":"PRODUCTS_PAYLOAD"
+					},
+					{
+						"title":"About us",
+						"type":"web_url",
+						"url":"https://magixshop.com/pages/about-magix-shop",
+					},
+					{
+						"title":"Contact us",
+						"type":"postback",
+						"payload":"CONTACT_PAYLOAD"
+					}]
+				},
+				{
+					"type":"web_url",
+					"title":"Visit Shop",
+					"url":"https://magixshop.com/",
+					"webview_height_ratio":"full"
+				}]
+			}]
+		}
+	}
+	sendMessageByUserId(sender, messageData)
 }
 
 function sendMessageByUserId(sender, message) {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { 
-                id: sender 
-            },
-            message: message,
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: { access_token: token },
+		method: 'POST',
+		json: {
+			recipient: { 
+				id: sender 
+			},
+			message: message,
+		}
+	}, function (error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
 }
 
 function sendMessageByUserReg(user_ref, message, reply) {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { 
-                user_ref: user_ref
-            },
-            message: message,
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-            reply.status(400).send(error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-            reply.status(400).send(response.body.error)
-        } else {
-            reply.status(200).send(body)
-        }
-    })
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: { access_token: token },
+		method: 'POST',
+		json: {
+			recipient: { 
+				user_ref: user_ref
+			},
+			message: message,
+		}
+	}, function (error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+			reply.status(400).send(error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+			reply.status(400).send(response.body.error)
+		} else {
+			reply.status(200).send(body)
+		}
+	})
 }
 
 // Spin up the server
 app.listen(app.get('port'), function () {
-    console.log('running on port', app.get('port'))
+	console.log('running on port', app.get('port'))
 })
